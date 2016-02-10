2ab0461202050679189fc99cd4d47057121abc08
Danyiar Turak <danyiarturak@gmail.com>
2016-02-11 02:27:35 +0600

    Price

---
+++ PriceFormatter/ClassPrice.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceFormatter
+{
+    class ClassPrice
+    {
+        private double price;
+
+
+        public ClassPrice()
+        {
+            this.price = 0;
+        }
+
+        public ClassPrice(double price)
+        {
+            this.price = price;
+        }
+        
+        public double GetPrice()
+        {
+            return this.price;
+        }
+
+        public override string ToString()
+        {
+            String strPrice = " " + this.price; // пустоой строкага енгиздим
+            int cnt = 0;
+            for (int i = 0; i < strPrice.Length; i++)
+            {
+                cnt++;
+                if ((strPrice - cnt) % 3 == 0)
+                {
+                    p = strPrice.Insert(i, " ");
+                }
+            }
+        
+            return price + "тг";
+        }
+
+        public static ClassPrice operator +(ClassPrice left, ClassPrice right)
+        {
+            return new ClassPrice(left.price + right.price);
+        }
+    }
+}
---
+++ PriceFormatter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceFormatter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Defolt constructor
+            //Constructor
+            //Getter
+            //ToString
+
+            ClassPrice price = new ClassPrice(double.Parse(Console.ReadLine()));
+            Console.WriteLine(price);
+
+            ClassPrice price2 = new ClassPrice(350.43);
+            Console.Write
+        }
+    }
+}
